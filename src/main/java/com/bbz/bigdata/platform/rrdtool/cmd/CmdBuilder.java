21c2676ff148faac5679d2de4d0b3f2babfba142
weiran <weiran22222@163.com>
2016-06-01 16:52:11 +0800

    将Measurement实现单例放到了Metrics类中

--- src/main/java/com/bbz/bigdata/platform/rrdtool/cmd/CmdBuilder.java
+++ src/main/java/com/bbz/bigdata/platform/rrdtool/cmd/CmdBuilder.java
@@ -6,26 +6,28 @@ import com.bbz.bigdata.platform.rrdtool.cmd.cmds.DiskSearchCmd;
 import com.bbz.bigdata.platform.rrdtool.cmd.cmds.MemorySearchCmd;
 import com.bbz.bigdata.platform.rrdtool.cmd.cmds.NetworkSearchCmd;
 import com.bbz.bigdata.platform.rrdtool.measurement.Measurement;
+import com.bbz.bigdata.platform.rrdtool.measurement.Metrics;
 
 public class CmdBuilder {
 	
 	/**
 	 * 创建cmd命令服务类
-	 * @param hostName
-	 * @param measurement
-	 * @param startTime
-	 * @param endTime
-	 * @return
+	 * @param clusterName          集群名称
+	 * @param hostName 主机名称，空字符串表示sumarryinfo
+	 * @param measurement  测量详细量数组 [Metrics.CPU.Free,Metrics.Memory.Total]
+	 * @param startTime 开始时间 MM/dd/yyyy HH:mm
+	 * @param endTime 结束时间 MM/dd/yyyy HH:mm
+	 * @return 查询指令对象
 	 */
-	public static ICmd buildCmd( String hostName, Measurement measurement, String startTime, String endTime){
-		if (measurement==Measurement.Memory) {
-			return new MemorySearchCmd(hostName, startTime, endTime);
-		}else if (measurement==Measurement.CPU) {
-			return new CPUSearchCmd(hostName, startTime, endTime);
-		}else if (measurement==Measurement.Network) {
-			return new NetworkSearchCmd(hostName, startTime, endTime);
-		}else if (measurement==Measurement.Disk) {
-			return new DiskSearchCmd(hostName, startTime, endTime);
+	public static ICmd buildCmd(String clusterName, String hostName, Measurement measurement, String startTime, String endTime){
+		if (measurement==Metrics.Memory) {
+			return new MemorySearchCmd(clusterName, hostName, startTime, endTime);
+		}else if (measurement==Metrics.CPU) {
+			return new CPUSearchCmd(clusterName, hostName, startTime, endTime);
+		}else if (measurement==Metrics.Network) {
+			return new NetworkSearchCmd(clusterName, hostName, startTime, endTime);
+		}else if (measurement==Metrics.Disk) {
+			return new DiskSearchCmd(clusterName, hostName, startTime, endTime);
 		}else {
 			return null;
 		}
